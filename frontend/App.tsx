491b8176e28cfa922524f8c9e92c8ba9dfc5f070
zknnz <kunanon.smm@mail.kmutt.ac.th>
2024-02-21 15:17:16 +0700

    FeedPage

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -6,6 +6,7 @@
  */
 
 import React from 'react';
+import Feed_Page from './Feed_Page';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -63,36 +64,7 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <Feed_Page />
   );
 }
 
