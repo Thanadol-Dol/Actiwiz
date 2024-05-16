b2824d425a2c5845069505b785f24c5ee4072319
Dol <thanadol.thongrit@gmail.com>
2024-05-16 14:49:55 +0700

    Fix navigate to detail page

--- frontend/screens/FeedPageEvent.tsx
+++ frontend/screens/FeedPageEvent.tsx
@@ -171,22 +171,6 @@ const FeedPageEvent = ({navigation}: {navigation: any}) => {
     setSearchText(query);
   };
   
-  const navigateToDetailPage = (item: ActivityDetail) => {
-    navigation.navigate('DetailPage', { 
-      "ActivityID": item.ActivityID ,
-      "ActivityName": item.ActivityName,
-      "ActivityNameENG": item.ActivityNameENG, 
-      "Description": item.Description,
-      "HourTotal": item.HourTotal,
-      "DayTotal": item.DayTotal,
-      "Semester": item.Semester,
-      "Organizer": item.Organizer,
-      "OpenDate": item.OpenDate,
-      "CloseDate": item.CloseDate,
-      "AcademicYear": item.AcademicYear,
-    });
-  };
-  
   const renderSearchItem = ({ item }: { item: ActivityDetail }) => (
     <SearchEventCard key={item.ActivityID} navigation={navigation} event={item}/>
   );
