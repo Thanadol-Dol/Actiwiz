c75fd8847384956892a4ac2662945df183dc9d7d
Dol <thanadol.thongrit@gmail.com>
2024-05-20 15:25:06 +0700

    Adding token renewal function in util

--- frontend/utils/tokenUtils.ts
+++ frontend/utils/tokenUtils.ts
@@ -1,4 +1,15 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const removeTokens = async () => {
+    try {
+      await AsyncStorage.removeItem("apiToken");
+      await AsyncStorage.removeItem("graphToken");
+      await AsyncStorage.removeItem("refreshToken");
+    } catch (error) {
+      throw error;
+    }
+}
 
 const refreshApiToken = async (refreshToken : string | null): Promise<any> => {
     try {
@@ -25,3 +36,26 @@ const refreshGraphToken = async (refreshToken : string | null): Promise<any> => 
       throw error;
     }
 };
+
+const setNewTokens = async () => {
+  try{
+    const refreshToken = await AsyncStorage.getItem("refreshToken");
+
+    await AsyncStorage.removeItem("apiToken");
+    await AsyncStorage.removeItem("graphToken");
+    const api_token = await refreshApiToken(refreshToken);
+    console.log("api_token:", api_token.api_token);
+    await AsyncStorage.setItem("apiToken", api_token.api_token)
+
+    const tokens = await refreshGraphToken(refreshToken);
+    console.log("graph_token:", tokens.graph_token);
+    console.log("refresh_token:", tokens.refresh_token);
+    await AsyncStorage.setItem("graphToken", tokens.graph_token);
+    await AsyncStorage.setItem("refreshToken", tokens.refresh_token);
+  } catch (error) {
+    removeTokens();
+    throw error;
+  }
+}
+
+export { refreshApiToken, refreshGraphToken, setNewTokens, removeTokens };
